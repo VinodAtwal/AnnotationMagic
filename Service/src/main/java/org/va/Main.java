d6c17d57295c67406210a3d826155d252e4090d3
Vinod Atwal <vinodatwal@Vinods-Laptop.local>
2025-01-26 22:52:56 +0530

    adding pid details for dynamic agent loading

--- Service/src/main/java/org/va/Main.java
+++ Service/src/main/java/org/va/Main.java
@@ -3,12 +3,13 @@ package org.va;
 import org.va.runner.RepositoryScenariosRunner;
 import org.va.runner.SimpleAnnotationProcess;
 
+import java.time.Duration;
+
 public class Main {
-    public static void main(String[] args) {
-      /*
-        compile time code gen runner
-      */
-      new RepositoryScenariosRunner().run();
+    public static void main(String[] args) throws InterruptedException {
+        long pid = ProcessHandle.current().pid();
+        System.out.println("Current PID: " + pid);
+        new RepositoryScenariosRunner().run();
       /*
       simple annotation runner
       */
